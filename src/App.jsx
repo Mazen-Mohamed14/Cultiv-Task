3888ae51f10196a0347abbe97357a2e4698a128e
Mazen Mohamed <mazenmezomohammed@gmail.com>
2024-01-24 23:09:44 +0200

    refactor: Added navbar and outlet to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import { Navbar } from "./components/Navbar.jsx";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <h1> Hmada</h1>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </div>
+  );
 }
 
 export default App;
