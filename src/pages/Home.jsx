a1452f31018e3821f02c39f21b20775ef10c6d94
Mazen Mohamed <mazenmezomohammed@gmail.com>
2024-01-27 23:22:50 +0200

    feat: build card rendering functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Post } from "../components/Post";
 
 const Home = () => {
   const [posts, setPosts] = useState({});
@@ -10,14 +11,28 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="home-background">
-      {posts?.blogs?.map((post) => {
-        return (
-          <div>
-            <img src={post.photo_url}></img>
-          </div>
-        );
-      })}
+    <div className="album py-5 mt-4 home-background">
+      <div className="container">
+        <div className="row mx-auto row-cols-lg-2 row-cols-xl-3 g-5">
+          {posts?.blogs?.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                id={post.id}
+                source={post.photo_url}
+                title={post.title}
+                desc={post.description}
+                text={
+                  post.content_text.slice(
+                    0,
+                    post.content_text.substring(0, 130).lastIndexOf(".")
+                  ) + "."
+                }
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
