ae60b8d791625ec494429b54ad0352db9c97d0a5
Mazen Mohamed <mazenmezomohammed@gmail.com>
2024-01-28 00:39:36 +0200

    fix: replace bootstrap container with vanilla grid container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,25 +13,23 @@ const Home = () => {
   return (
     <div className="album py-5 mt-4 home-background">
       <div className="grid-container">
-        <div className="row mx-auto row-cols-lg-2 row-cols-xl-3 g-5">
-          {posts?.blogs?.map((post) => {
-            return (
-              <Post
-                key={post.id}
-                id={post.id}
-                source={post.photo_url}
-                title={post.title}
-                desc={post.description}
-                text={
-                  post.content_text.slice(
-                    0,
-                    post.content_text.substring(0, 130).lastIndexOf(".")
-                  ) + "."
-                }
-              />
-            );
-          })}
-        </div>
+        {posts?.blogs?.map((post) => {
+          return (
+            <Post
+              key={post.id}
+              id={post.id}
+              source={post.photo_url}
+              title={post.title}
+              desc={post.description}
+              text={
+                post.content_text.slice(
+                  0,
+                  post.content_text.substring(0, 130).lastIndexOf(".")
+                ) + "."
+              }
+            />
+          );
+        })}
       </div>
     </div>
   );
